4f9a456116bddae4f42dcddc28f01c6571022265
alaaraafeh <alaaraafeh@gmail.com>
2025-12-19 10:16:39 +0200

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+
+//test 1
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+
+
+
+
+// test 2
+
+test("button file snapshot", () => {
+  const { container } = render(<button>New</button>);
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <button>
+      New
+    </button>
+  `);
+
+  // expect(container.firstChild).toMatchSnapshot(); 
+}); 
